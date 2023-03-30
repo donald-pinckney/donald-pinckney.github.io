86a054daf9213a32a2b27fc30079766d89ef97cf
Donald Pinckney <donald_pinckney@icloud.com>
2023-03-29 21:34:18 -0400

    Deploy to GitHub Pages

--- public/js/new_tab.js
+++ public/js/new_tab.js
@@ -1,8 +1,8 @@
 var links = document.links;
 
 for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-	var type = links[i].href.split('.').pop();
-    if (type == 'pdf') {
+    var type = links[i].href.split('.').pop();
+    if (type == 'pdf' || links[i].href.includes("arxiv")) {
         links[i].target = '_blank';
     }
 }
